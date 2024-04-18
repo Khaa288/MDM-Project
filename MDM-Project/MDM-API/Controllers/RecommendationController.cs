956275da15a7c2054c0b7eac9cf4d14f587ca9f6
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2024-04-18 20:08:44 +0700

    Add recommendation controller

--- MDM-Project/MDM-API/Controllers/RecommendationController.cs
+++ MDM-Project/MDM-API/Controllers/RecommendationController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MDM_API.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Neo4j.Driver;
 
@@ -13,6 +13,18 @@ namespace MDM_API.Controllers
         public RecommendationController(IAsyncSession session)
         {
             _session = session;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> RecommendTrip (string userId)
+        {
+            var recommendTrips = new List<IRecord>();
+
+            var neo4j_RECOMMENDATION1 = await _session.RunAsync(RecommendationQueries.RECOMMENDATION1, new { maTaiKhoan = userId });
+            var neo4j_RECOMMENDATION2 = await _session.RunAsync(RecommendationQueries.RECOMMENDATION2, new { maTaiKhoan = userId });
+            var neo4j_RECOMMENDATION3 = await _session.RunAsync(RecommendationQueries.RECOMMENDATION3);
+
+            return Ok(neo4j_RECOMMENDATION2);
         }
     }
 }
---
+++ MDM-Project/MDM-API/Utilities/RecommendationQueries.cs
@@ -0,0 +1,31 @@
+﻿namespace MDM_API.Utilities
+{
+    public class RecommendationQueries
+    {
+        // Recommend top 3 trip based on the most frequently booking tickets
+        public const string RECOMMENDATION1 =
+            "MATCH (tk: TaiKhoan {MaTaiKhoan: $maTaiKhoan})-[r:DaDat]->(cx:ChuyenXe) " +
+            "RETURN r.SoLan, cx.MaChuyen " +
+            "ORDER BY r.SoLan DESC " +
+            "LIMIT 3 ";
+
+        // Recommend top 5 trips based on top 3 latest tickets -> get locations -> Top [2 -> 10] new trip based on the locations
+        public const string RECOMMENDATION2 =
+            "MATCH p = (tk:TaiKhoan {MaTaiKhoan: $maTaiKhoan})-[r:Dat]->(cx:ChuyenXe)-[]->(dd:DiaDiem)<-[]-(gy:ChuyenXe) " +
+            "WHERE cx.MaChuyen <> gy.MaChuyen " +
+            "RETURN dd.TenDiaDiem, collect(gy.MaChuyen)[0..2] ";
+
+        // Recommend top 3 trips based on top 1 locations pairs with highest "CungDat" relationship number 
+        public const string RECOMMENDATION3 =
+            "CALL { " +
+                "MATCH (dd1:DiaDiem)-[r:CungDat]->(dd2:DiaDiem) " +
+                "RETURN dd1, dd2 " +
+                "ORDER BY r.SoLan DESC " +
+                "LIMIT 1 " +
+            "} " +
+            "MATCH (origin:DiaDiem)<-[:CoDiemDi]-(cx:ChuyenXe)-[:CoDiemDen]->(d2:des) " +
+            "WHERE (origin = dd1 and des = dd2) or (origin = dd2 and des = dd1) " +
+            "RETURN cx " +
+            "LIMIT 3 ";
+    }
+}
