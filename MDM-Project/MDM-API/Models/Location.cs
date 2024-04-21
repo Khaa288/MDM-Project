8b071dad6d2dceac21d128ed29506d80332d0b6c
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2024-04-21 16:56:54 +0700

    Remove unecessary Models

--- MDM-Project/MDM-API/Models/Location.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MDM_API.Models
-{
-    public class Location
-    {
-        public string MaDiaDiem { get; set; }
-        public string TenDiaDiem { get; set; }
-    }
-}
--- MDM-Project/MDM-API/Models/Trip.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MDM_API.Models
-{
-    public class Trip
-    {
-        public string MaTuyen { get; set; }
-        public string TenTuyen { get; set;}
-    }
-}
